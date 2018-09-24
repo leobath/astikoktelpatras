87c54697c5eb6de0536382c3151c7829c917d175
leo bath <leoneedass@gmail.com>
2018-09-24 22:28:16 +0000

    jquery

--- public/javascripts/customScript.js
+++ public/javascripts/customScript.js
@@ -5,9 +5,21 @@ $('.map-container')
 			$(this).find('iframe').removeClass('clicked')});
 
 function blinker() {
-    $('.blink_me').fadeOut(500);
     $('.blink_me').fadeIn(500);
 }
 
 setInterval(blinker, 1000); //Runs every second
+$(document).ready(function(){
+    $("#value-one").click(function(){
+        $("#bus-hours").hide();
+    });
+    $(".btn2").click(function(){
+        $("p").show();
+    });
+});
 
+ $("form").on("submit", function() {
+      // store the value of the input with name='age' 
+       var age = $(this).find('[name=myfield]').val();
+       $("#bus-hours").hide();
+    });
