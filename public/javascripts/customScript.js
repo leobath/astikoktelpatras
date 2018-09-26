27ac664a82119a70362c401c3f2789aff58a934b
leo bath <leoneedass@gmail.com>
2018-09-26 16:14:51 +0000

    fix back button and selector

--- public/javascripts/customScript.js
+++ public/javascripts/customScript.js
@@ -12,23 +12,66 @@ setInterval(blinker, 2000); //Runs every second
 
 // Timetables departure to different destinations
 
-let timetableHTML = ["ΤΕΡΨΙΘΕΑ-ΕΓΛΥΚΑΔΑ timetable", "ΤΕΡΨΙΘΕΑ-ΔΙΑΚΟΥ"];
 
 
+var eglykada = ` 
+  <p><b><u>ΚΑΘΗΜΕΡΙΝΕΣ</b></u>:<br>
+&#x25CF Από ΡΟΜΑΝΤΖΑ για ΕΓΛΥΚΑΔΑ: 06.20 – 06.50 – 07.00<br>
+&#x25CF Από ΕΓΛΥΚΑΔΑ για ΡΟΜΑΝΤΖΑ: 06.20 – 06.40 – 07.00<br>
+&#x25CF Από 07.00 έως 21.00 ανά 10 λεπτά<br>
+&#x25CF Από 21.00 έως 22.40 ανά 20 λεπτά<br>
+Σημ. ΔΕΥΤΕΡΑ & ΤΕΤΑΡΤΗ ΑΠΟΓΕΥΜΑ: από 15.00 έως 21.00 ανά 12 λεπτά<br>
+<hr>
+<b><u>ΣΑΒΒΑΤΟ</b></u>:<br>
+&#x25CF Από ΡΟΜΑΝΤΖΑ για ΕΓΛΥΚΑΔΑ: 06.30 – 06.50 – 07.05<br>
+&#x25CF Από ΕΓΛΥΚΑΔΑ για ΡΟΜΑΝΤΖΑ: 06.30 – 06.50 – 07.00<br>
+&#x25CF Από 07.00 έως 15.00 ανά 10 λεπτά<br>
+&#x25CF Από 15.00 έως 20.30 ανά 15 λεπτά<br>
+&#x25CF Από 20.30 έως 22.40 ανά 20 λεπτά<br>
+<hr>
+<b><u>ΚΥΡΙΑΚΗ & ΑΡΓΙΕΣ</b></u>:<br>
+&#x25CF Από 07.00 έως 15.00 ανά 30 λεπτά<br>
+&#x25CF Από 15.00 έως 22.40 ανά 20 λεπτά<br>
+  </p>
+`;
+
+var diakou = `
+              <p><b><u>ΚΑΘΗΜΕΡΙΝΕΣ</u></b>:<br>
+Από ΡΟΜΑΝΤΖΑ: 06.30 – 07.10 (ΛΥΚΟΧΩΡΟ) - 08.00 – 09.00 – 11.00 – 13.00 – 14.00 (ΛΥΚΟΧΩΡΟ)
+– 16.00 – 18.00 – 20.00 – 21.00<br>
+Από ΔΙΑΚΟΥ: 07.05 – 07.50 - 08.40 – 09.40 – 11.40 – 13.40 – 14.40 – 16.40 – 18.40 – 20.40 – 21.40<br>
+<hr>
+<b><u>ΣΑΒΒΑΤΟ</u></b>:<br>
+Από ΡΟΜΑΝΤΖΑ: 07.05 – 09.00 – 11.00 – 14.00 – 16.00 – 18.00 – 20.00<br>
+Από ΔΙΑΚΟΥ: 07.45 – 09.40 – 11.40 – 14.40 – 16.40 – 18.40 – 20.40<br>
+<hr>
+<b><u>ΚΥΡΙΑΚΗ & ΑΡΓΙΕΣ</u></b>:<br>
+Από ΡΟΜΑΝΤΖΑ: 07.40 – 08.40 – 10.00 – 14.00 - 16.00 – 18.00 – 20.00<br>
+Από ΔΙΑΚΟΥ: 08.20 – 09.20 – 10.40 – 14.40 - 16.40 – 18.40 – 20.40 <br>
+
+      `;
+      
+var elekistra = `
+  <p>
+    <b><u>ΚΑΘΗΜΕΡΙΝΕΣ</u></b>:<br>
+    από ΕΛΕΚΙΣΤΡΑ ώρα 6.55 <br>
+    από ΡΩΜΑΝΟΥ ώρα 7.05 <br>
+    από ΡΟΜΑΝΤΖΑ ώρα 14.30<br>
+    <hr>
+    <b><u>ΚΥΡΙΑΚΗ & ΑΡΓΙΕΣ</u></b>:<br>
+    Δεν εκτελούνται δρομολόγια 
+  </p>
+`;
 
 
 function displayVals() {
-  var eglykada = "hi";
-    
-  var diakou = `
-               hello there
-      `;
+  
       
   var singleValues = $( "#single" ).val();
   
 if(singleValues == 0){
   $( "#timetable" ).html('');
-  $("#timetable-title").html("");
+
   $( '#iframe' ).attr( 'src', function ( i, val ) { return ''; });
   $(".map-background").css("opacity", 0.1);
   $('.map-background').css("background-image", "url(\"/svgs/solid/map-marked-alt.svg\")");
@@ -37,20 +80,29 @@ if(singleValues == 0){
   else if(singleValues === 'eglykada') {
     
     $( "#timetable" ).html(eglykada);
-    $("#timetable-title").html("<h5>Ώρες Αναχώρησης</h5>");
+  
     $( '#iframe' ).attr( 'src', function ( i, val ) { return 'https://www.google.com/maps/d/embed?mid=1LP-JieqtwB0htg5y8lecc580J0SqwzkO'; });
     $(".map-background").css("background-image", "none");
     $(".map-background").css("opacity", 1);
-    $("#route-destination").html("<b>ΤΕΡΨΙΘΕΑ-ΕΓΛΥΚΑΔΑ");
+    $("#route-destination").html("<b>ΕΓΛΥΚΑΔΑ</b>");
    
   }
   else if (singleValues === 'diakou') {
-    $("#timetable-title").html("<h5>Ώρες Αναχώρησης</h5>");
+  
     $( "#timetable" ).html(diakou);    
+    $( '#iframe' ).attr( 'src', function ( i, val ) { return 'https://www.google.com/maps/d/embed?mid=1Z1rcynjOSOcXCTgqGiuVv-Tj8ADX8RlY&z=14'; });
+    $(".map-background").css("background-image", "none");
+    $(".map-background").css("opacity", 1);
+    $("#route-destination").html("<b>ΔΙΑΚΟΥ</b>");
+
+  }
+  else if (singleValues === 'elekistra') {
+    $("#timetable-title").html("<h5>Ώρες Αναχώρησης</h5>");
+    $( "#timetable" ).html(elekistra);    
     $( '#iframe' ).attr( 'src', function ( i, val ) { return 'https://www.google.com/maps/d/embed?mid=1Z1rcynjOSOcXCTgqGiuVv-Tj8ADX8RlY'; });
     $(".map-background").css("background-image", "none");
     $(".map-background").css("opacity", 1);
-    $("#route-destination").html("<b>ΤΕΡΨΙΘΕΑ-ΔΙΑΚΟΥ");
+    $("#route-destination").html("<b>ΕΛΕΚΙΣΤΡΑ</b>");
 
   }
   
